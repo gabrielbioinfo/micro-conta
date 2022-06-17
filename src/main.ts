649ab98e511fe909000df4b8cdc4ae48cb3b5a59
Gabriel Espindola <gabrielbioinfo@gmail.com>
2022-06-17 19:34:53 -0300

    feat: config eslint

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-
-class MeuTeste{
+class MeuTeste {
   private name = 'Meu Teste';
 
-  static create(){
+  static create() {
     return new this();
   }
 
   sayMyName = () => {
     console.log(`Say my name, say my name: ${this.name}`);
-  }
+  };
 }
 
 console.log(MeuTeste.create().sayMyName());
