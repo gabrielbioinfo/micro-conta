a40802398fb980d5b11d1d5d3b0a83c4ea3dbeba
Gabriel Espindola <gabrielbioinfo@gmail.com>
2022-06-18 02:20:37 -0300

    feat: adds new strings

--- src/_shared/i18n/strings/error.ts
+++ src/_shared/i18n/strings/error.ts
@@ -1,5 +1,12 @@
 const ERROR = {
   INVALID_UUID: 'errors.invalid.uuid',
+  INVALID_CPF: 'errors.invalid.cpf',
+  INVALID_CPF_EMPTY: 'errors.invalid.cpf.empty',
+  INVALID_CPF_REJECTED: 'errors.invalid.cpf.rejected',
+  INVALID_CPF_ALLTHESAME: 'errors.invalid.cpf.allthesame',
+  INVALID_CPF_NOT_UNIQUE: 'errors.invalid.cpf.notunique',
+  INVALID_FULLNAME: 'errors.invalid.fullname',
+  INVALID_FULLNAME_EMPTY: 'errors.invalid.fullname.empty',
 };
 
 export default ERROR;
